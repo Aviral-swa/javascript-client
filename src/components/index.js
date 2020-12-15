a5c7ffb315c45af7c75a111cd7003734aad02efc
Aviral Swarnkar <aviral.swarnkar@successive.com>
2020-12-16 02:40:51 +0530

    feat: add math component & implement props.children(#40791)

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Slider, Div } from './Slider';
 export { SelectField } from './SelectField';
 export { RadioGroup } from './RadioGroup';
 export { Button } from './Button';
+export { Math } from './Math';
---
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Math } from '../../components';
+
+const ChildrenDemo = () => (
+  <>
+    <Math
+      first={91}
+      second={63}
+      operator="*"
+    >
+      {
+        (item) => (
+          <p>
+            {' '}
+            Multiplication of
+            {' '}
+            {item.first}
+            {' '}
+            and
+            {' '}
+            {item.second}
+            {' '}
+            is
+            {' '}
+            {item.result}
+          </p>
+        )
+      }
+    </Math>
+    <Math first={4} second={6} operator="+">
+      {
+        (item) => (
+          <p>
+            When we add
+            {' '}
+            {item.first}
+            {' '}
+            &
+            {' '}
+            {item.second}
+            {' '}
+            we get
+            {' '}
+            {item.result}
+          </p>
+        )
+      }
+    </Math>
+    <Math first={4} second={0} operator="/">
+      {
+        (item) => (
+          <p>
+            When we divide
+            {' '}
+            {item.first}
+            {' '}
+            &
+            {' '}
+            {item.second}
+            {' '}
+            we get
+            {' '}
+            {item.result}
+          </p>
+        )
+      }
+    </Math>
+    <Math first={4} second={6} operator="?">
+      {
+        (item) => (
+          <p>
+            When we perform (?)
+            {' '}
+            {item.first}
+            {' '}
+            &
+            {' '}
+            {item.second}
+            {' '}
+            we get
+            {' '}
+            {item.result}
+          </p>
+        )
+      }
+    </Math>
+    <Math
+      first={2}
+      second={3}
+      operator="+"
+    />
+  </>
+);
+
+export default ChildrenDemo;

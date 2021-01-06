65835f72064a5a6f421d9cd687a8fca61258bd9c
Aviral Swarnkar <aviral.swarnkar@successive.com>
2021-01-06 11:23:16 +0530

    feat: remove checks on authRoute(#41251)

--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AuthLayout } from '../layouts';
 import { NoMatch } from '../pages';
 
-const AuthRoute = () => {
-  if (!localStorage.getItem('token')) {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/login" render={(routerProps) => <AuthLayout history={routerProps.history} />} />
-          <Route default component={NoMatch} />
-        </Switch>
-      </div>
-    );
-  }
-  return (
+const AuthRoute = () => (
+  <div>
     <Switch>
-      <Redirect to="/add-trainee" />
+      <Route exact path="/login" render={(routerProps) => <AuthLayout history={routerProps.history} />} />
+      <Route default component={NoMatch} />
     </Switch>
-  );
-};
+  </div>
+);
 
 export default AuthRoute;

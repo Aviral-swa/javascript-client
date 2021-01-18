c7aa2d4d84fbf66bc95c3777bc7b5c7a97dacc7d
Aviral Swarnkar <aviral.swarnkar@successive.tech>
2021-01-18 13:42:17 +0530

    refac: improved flow for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import theme from './theme';
 import { PrivateRoute, AuthRoute } from './routes';
 import { ProvideSnackBar } from './contexts';
+import {
+  ChildrenDemo, TextFieldDemo, InputDemo,
+  NoMatch, Login, Trainee,
+} from './pages';
 
 const App = () => (
   <ThemeProvider theme={theme}>
@@ -12,8 +16,13 @@ const App = () => (
     <ProvideSnackBar>
       <Router>
         <Switch>
-          <Route exact path="/login" component={AuthRoute} />
-          <Route default component={PrivateRoute} />
+          <Redirect exact path="/" to="/login" />
+          <AuthRoute exact path="/login" component={Login} />
+          <PrivateRoute exact path="/input-demo" component={InputDemo} />
+          <PrivateRoute exact path="/children-demo" component={ChildrenDemo} />
+          <PrivateRoute exact path="/textfield-demo" component={TextFieldDemo} />
+          <PrivateRoute path="/add-trainee" component={Trainee} />
+          <PrivateRoute default component={NoMatch} />
         </Switch>
       </Router>
     </ProvideSnackBar>
--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,15 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { func } from 'prop-types';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { AuthLayout } from '../layouts';
-import { NoMatch } from '../pages';
 
-const AuthRoute = () => (
-  <div>
-    <Switch>
-      <Route exact path="/login" render={(routerProps) => <AuthLayout history={routerProps.history} />} />
-      <Route default component={NoMatch} />
-    </Switch>
-  </div>
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(routerProps) => (
+      <AuthLayout>
+        <Component {...routerProps} />
+      </AuthLayout>
+    )}
+  />
 );
+
+AuthRoute.propTypes = {
+  component: func.isRequired,
+};
 
 export default AuthRoute;

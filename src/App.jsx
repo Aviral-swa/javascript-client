5c288a232e302777c8432db5dd10eb9443a6a4bf
Aviral Swarnkar <aviral.swarnkar@successive.tech>
2021-01-25 13:49:16 +0530

    feat: connect apollo server & replace axios with graphQl mutation in login(#58681)

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import { ApolloProvider } from '@apollo/client';
 import theme from './theme';
 import { PrivateRoute, AuthRoute } from './routes';
 import { ProvideSnackBar } from './contexts';
+import client from './libs/apollo-client';
 import {
   ChildrenDemo, TextFieldDemo, InputDemo,
   NoMatch, Login, Trainee,
@@ -14,17 +16,19 @@ const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <ProvideSnackBar>
-      <Router>
-        <Switch>
-          <Redirect exact path="/" to="/login" />
-          <AuthRoute exact path="/login" component={Login} />
-          <PrivateRoute exact path="/input-demo" component={InputDemo} />
-          <PrivateRoute exact path="/children-demo" component={ChildrenDemo} />
-          <PrivateRoute exact path="/textfield-demo" component={TextFieldDemo} />
-          <PrivateRoute path="/add-trainee" component={Trainee} />
-          <PrivateRoute default component={NoMatch} />
-        </Switch>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Switch>
+            <Redirect exact path="/" to="/login" />
+            <AuthRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/input-demo" component={InputDemo} />
+            <PrivateRoute exact path="/children-demo" component={ChildrenDemo} />
+            <PrivateRoute exact path="/textfield-demo" component={TextFieldDemo} />
+            <PrivateRoute path="/add-trainee" component={Trainee} />
+            <PrivateRoute default component={NoMatch} />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     </ProvideSnackBar>
   </ThemeProvider>
 );
